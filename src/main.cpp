90d6d828b4e75abec69c4d2c9852ae2f80eb569b
Bruno Carmo Nunes <brunocmo.nunes@gmail.com>
2022-12-05 23:46:31 -0300

    Add edges to nodes

--- src/main.cpp
+++ src/main.cpp
@@ -1,22 +1,11 @@
 #include <iostream>
 #include <unistd.h>
 
-#include "../inc/Graph.h"
+#include "../inc/Engine.h"
 
 int main() {
-
-  Graph teste;
-
-  teste.addEdge(1, 2, 52);
-  teste.addEdge(2, 3, 61);
-  teste.addEdge(1, 18, 154);
-  teste.addEdge(18, 19, 53);
-  teste.addEdge(19, 2, 171);
-  teste.addEdge(3, 4, 64);
-  teste.addEdge(3, 20, 165);
-  teste.addEdge(20, 19, 67);
-  teste.addEdge(20, 21, 57);
-  teste.addEdge(21, 4, 159); 
+ 
+  Engine teste;
 
   teste.printMap();
 
